08b0a18c2142e381360c11980b6b4f9949696173
yi-jou-huang <df467289@gmail.com>
2024-11-26 17:33:52 +0800

    Logout part finish

--- dcrm/myweb/urls.py
+++ dcrm/myweb/urls.py
@@ -4,7 +4,7 @@ from . import views # 從同一個資料夾(myweb)匯入其他檔案: views.py
 urlpatterns = [
     path('', views.home, name='home'),  
     # path('login/', views.login_user, name='login'),  # name='login' 可以讓我們html的 href="/login/"寫成href="{% url 'login' %}" 方便指認url裡的哪一個path(靠的就是'name'參數)
-    # path('logout/', views.logout_user, name='logout'),  
+    path('logout/', views.logout_user, name='logout'),  
     
 ]
 # views.home > views.py裡有 'home' function
--- dcrm/myweb/views.py
+++ dcrm/myweb/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout # 使用者'身分驗證'及'登入、登出'
-from django.contrib import messages # 想要在註冊時、登入、登出時出現一個訊息告知你現在的狀態如你已登入、你登出等訊息
+from django.contrib import messages # 設置提示訊息，顯示狀態或錯誤通知，例如成功登入或錯誤訊息
 
 def home(request):
-    # 前端form表單送出請求給後端，如果請求/request的方法是post 就 do something in here
+    # 前端form表單送出請求給後端，如果請求/request是 post 方法,
     if request.method == 'POST':
         # do something in here
-        username = request.POST['username'] # 'username'是前端form表單的<input>裡的'name'屬性帶有'username'
+        username = request.POST['username'] 
         password = request.POST['password']
-        # print(username) # request.POST <calss QueryDict> {'csrfmiddlewaretoken': ['O2iB6jWRcGha2ljj0hcMQPSUTv5lvylRTyoCi4rMqJdS316Veu3APaSXdpVCxh19'], 'username': ['Cathy'], 'password': ['16']}
+        # 從 request.POST（一個 QueryDict 對象）中提取前端表單的 username 和 password。這些鍵對應於表單中 <input> 元素的 name 屬性。 
+        # <calss QueryDict> {'csrfmiddlewaretoken': ['O2iB6jWRcGha2ljj0hcMQPSUTv5lvylRTyoCi4rMqJdS316Veu3APaSXdpVCxh19'], 'username': ['Cathy'], 'password': ['16']}
         # Authenticate
-        user = authenticate(request, username=username, password=password) # authenticate(request, **keywords) return User object
+        user = authenticate(request, username=username, password=password) 
+        # authenticate(request, **keywords) 檢查使用者憑證是否正確，返回 User 對象（如果正確）或 None。
+        # 檢查 username 和 password 是否正確。如果正確，返回對應的 User 對象；否則返回 None。
         if user is not None:
-            login(request,user) # login(request,user)
-            messages.success(request, 'You have been logged in.') # messages.success(request, message)
+            login(request,user) 
+            # login(request,user) 使用 login 函數登入使用者
+            messages.success(request, 'You have been logged in.') 
+            # messages.success(request, message)
             return redirect('home')
+            # 使用 redirect('home') 重定向到首頁（避免重新提交表單的問題）。
         else:
             messages.success(request, 'There was an error logged in, please try again.')
             return redirect('home')
-    else: # 如果前端沒有送出請求 畫面仍是 home.html
+    else: # 如果前端送出請求是 get 方法 畫面仍是 home.html
         return render(request, 'home.html', {})
+    
+def logout_user(request):
+    # 使用django的登出方法logout()
+    logout(request)
+    # 提示訊息
+    messages.success(request, 'You have been logged out ...')
+    return redirect('home')
