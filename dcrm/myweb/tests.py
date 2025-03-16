e5cf919f869f44cc2e94415be6e54ab3d8d4eb5a
yi-jou-huang <df467289@gmail.com>
2025-03-16 18:14:48 +0800

    add selenium auto-test

--- dcrm/myweb/tests.py
+++ dcrm/myweb/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Create your tests here.
+driver = webdriver.Chrome()
+driver.get("http://127.0.0.1:8080/")
+
+# 輸入帳號密碼
+driver.find_element(By.NAME, "username").send_keys("admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.ID, "login").click()
+# 確保登入成功
+h1_text = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "main-header"))
+)
+assert "Records..." in h1_text.text
+
+driver.quit()
